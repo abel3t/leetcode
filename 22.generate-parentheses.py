c785124ae2d5b5cd3d82617c29245791f49dbda8
Abel Tran <abeltran.develop@gmail.com>
2021-09-12 20:38:10 +0700

    Update

---
+++ 22.generate-parentheses.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=22 lang=python3
+#
+# [22] Generate Parentheses
+#
+
+# @lc code=start
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+        def generate(sofar="", op = 0, cl = 0):
+            if op > n: return
+            if cl > op: return
+            if cl == op == n:
+                result.append(sofar)
+            
+            generate(sofar + "(", op + 1, cl)
+            generate(sofar + ")", op, cl + 1)
+        generate()
+        return result
+
+
+
+# @lc code=end
+
