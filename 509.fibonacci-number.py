3aaa0a4f42d0d525dc1e6e4859418d9738d920f2
Abel Tran <abeltran.develop@gmail.com>
2021-09-14 21:13:51 +0700

    Add 509. Fibonacci Number solution

---
+++ 509.fibonacci-number.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=509 lang=python3
+#
+# [509] Fibonacci Number
+#
+
+# @lc code=start
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 3:
+            return 1
+        prev1, prev2 = 1, 1
+        for i in range(2, n):
+            prev1, prev2 = prev1 + prev2, prev1
+        return prev1
+        
+# @lc code=end
+
