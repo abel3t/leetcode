13feded2b92d40fc2c5b9e7178c87b3e291459ab
Abel Tran <abeltran.develop@gmail.com>
2021-09-14 21:29:31 +0700

    Update 509. Fibonacci Number

--- 509.fibonacci-number.py
+++ 509.fibonacci-number.py
@@ -7,8 +7,8 @@
 # @lc code=start
 class Solution:
     def fib(self, n: int) -> int:
-        if n < 3:
-            return 1
+        if n < 1: return 0
+        if n < 3: return 1
         prev1, prev2 = 1, 1
         for i in range(2, n):
             prev1, prev2 = prev1 + prev2, prev1
