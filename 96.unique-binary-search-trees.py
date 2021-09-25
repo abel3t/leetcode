8133681e47b54833ee4dd59141dffa30aaebc23d
Abel Tran <abeltran.develop@gmail.com>
2021-09-25 18:50:57 +0700

    Add 96. 96.unique-binary-search-trees.py solution

---
+++ 96.unique-binary-search-trees.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=96 lang=python3
+#
+# [96] Unique Binary Search Trees
+#
+
+# @lc code=start
+class Solution:
+    def numTrees(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0] = dp[1] = 1
+        
+        for i in range(2, n + 1):
+            for j in range(0, i):
+                dp[i] += dp[j] * dp[i-j-1]
+
+        return dp[n]
+# @lc code=end
+
