a09a06a1c7a5c3d4bd5accc08e1f83f404c4fdba
Abel Tran <abeltran.develop@gmail.com>
2021-09-25 19:10:33 +0700

    Update 70. Climbing stair

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -7,12 +7,12 @@
 # @lc code=start
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n < 3: return n
-        step_prev2 = 1
-        step_prev1 = 2
-        for i in range(2, n):
-            new = step_prev1 + step_prev2
-            step_prev1, step_prev2 = new, step_prev1
+        if n < 3:
+            return n
+
+        step_prev1, step_prev2 = 2, 1
+        for _ in range(2, n):
+            step_prev1, step_prev2 = step_prev1 + step_prev2, step_prev1
         return step_prev1
         
 # @lc code=end
