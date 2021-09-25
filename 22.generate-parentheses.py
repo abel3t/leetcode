9e67317a9a2e9ed21181aecebb0c5f92476640d0
Abel Tran <abeltran.develop@gmail.com>
2021-09-25 19:04:40 +0700

    Update 22. Generate Parentheses solution

--- 22.generate-parentheses.py
+++ 22.generate-parentheses.py
@@ -7,17 +7,21 @@
 # @lc code=start
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        result = []
-        def generate(sofar="", op = 0, cl = 0):
-            if op > n: return
-            if cl > op: return
-            if cl == op == n:
-                result.append(sofar)
-            
-            generate(sofar + "(", op + 1, cl)
-            generate(sofar + ")", op, cl + 1)
-        generate()
-        return result
+        if n < 1:
+            return [""]
+        elif n == 1:
+            return ["()"]
+        
+        current = set()
+        current.add("()")
+
+        for _ in range(2, n + 1):
+            next = set()
+            for s in current:
+                for i in range(len(s)):
+                    next.add(s[:i]+"()"+s[i:])
+            current = next
+        return list(current)
 
 # @lc code=end
 
