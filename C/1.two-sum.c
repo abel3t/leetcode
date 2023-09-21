b35d424af1d3a8cd5240fefd74b48e09e48ed8c2
Abel Tran <abeltran.develop@gmail.com>
2023-09-21 09:13:11 +0700

    Add: solve two sum problem in C

---
+++ C/1.two-sum.c
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=1 lang=c
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+
+typedef struct
+{
+  int number;
+  int idx;
+  // Makes structure hashable
+  UT_hash_handle hh;
+} num_t;
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *twoSum(int *nums, int numsSize, int target, int *returnSize)
+{
+  num_t *numsHash = NULL, *num = NULL, *tmp = NULL;
+
+  for (int i = 0; i < numsSize; ++i)
+  {
+    int toFind = target - nums[i];
+    HASH_FIND_INT(numsHash, &toFind, num);
+
+    if (num != NULL)
+    {
+      int *res = calloc((*returnSize = 2), sizeof(int));
+
+      res[0] = num->idx;
+      res[1] = i;
+
+      return res;
+    }
+
+    num = malloc(sizeof(num_t));
+    num->num = nums[i];
+    num->idx = i;
+
+    HASH_ADD_INT(numsHash, num, num);
+  }
+
+  HASH_ITER(hh, numsHash, num, tmp)
+  {
+    HASH_DEL(numsHash, num);
+    free(num);
+  }
+
+  return 0;
+}
+// @lc code=end
---
+++ C/README.md
@@ -0,0 +1,28 @@
+# LeetCode
+
+<!-- 
+<td style="color: rgb(0 184 163)">Easy</td>
+<td style="color: rgb(255 192 30)">Medium</td>
+<td style="color: rgb(255 55 95)">Hard</td>
+-->
+
+<table>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Title</th>
+      <th>Solution</th>
+      <th>Difficulty</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td><a href="https://leetcode.com/problems/two-sum/">Two Sum</a></td>
+      <td>
+        <a href="./1.two-sum.c">C</a></br>
+      </td>       
+      <td style="color: rgb(0 184 163)">Easy</td>
+    </tr>
+  </tbody>
+</table>
