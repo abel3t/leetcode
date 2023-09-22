3ff72ce5ae358f8662b2a663cf520f61338b2597
Abel Tran <abeltran.develop@gmail.com>
2023-09-22 09:30:14 +0700

    Add: solve roman to int

---
+++ C/13.roman-to-integer.c
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=13 lang=c
+ *
+ * [13] Roman to Integer
+ */
+
+// @lc code=start
+
+int decimalNumericalPlace(char roman)
+{
+  switch (roman)
+  {
+  case 'I':
+    return 1;
+
+  case 'V':
+    return 5;
+
+  case 'X':
+    return 10;
+
+  case 'L':
+    return 50;
+
+  case 'C':
+    return 100;
+
+  case 'D':
+    return 500;
+
+  case 'M':
+    return 1000;
+
+  default:
+    return -1;
+  }
+}
+
+int romanToInt(char *s)
+{
+  int sum = 0;
+
+  for (int i = 0; i < strlen(s); i++)
+  {
+    const currentValue = decimalNumericalPlace(s[i]);
+    const nextValue = decimalNumericalPlace(s[i + 1]);
+
+    if (currentValue < nextValue)
+    {
+      sum -= currentValue;
+    }
+    else
+    {
+      sum += currentValue;
+    }
+  }
+
+  return sum;
+}
+// @lc code=end
