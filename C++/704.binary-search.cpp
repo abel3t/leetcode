8c731ac6951bbef7a83b33714f704a124c71e9bd
Abel Tran <abeltran.develop@gmail.com>
2023-10-10 15:29:50 +0700

    add: solve 704 binary search

---
+++ C++/704.binary-search.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=704 lang=cpp
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+
+            if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
+// @lc code=end
