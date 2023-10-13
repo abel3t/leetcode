e21fd9129537a86687fd89a8fa243ca711ca16ea
Abel Tran <abeltran.develop@gmail.com>
2023-10-13 11:24:06 +0700

    add: solve 33. Search in Rotated Sorted Array

---
+++ C++/binary-search/33.search-in-rotated-sorted-array.cpp
@@ -0,0 +1,60 @@
+#include <vector>
+
+using namespace std;
+
+/*
+ * @lc app=leetcode id=33 lang=cpp
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int len = nums.size();
+        int left = 0;
+        int right = len - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid])
+            {
+                if (target >= nums[left] && target < nums[mid])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+
+                continue;
+            }
+
+            if (nums[mid] <= nums[len - 1])
+            {
+                if (target > nums[mid] && target <= nums[right])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+// @lc code=end
