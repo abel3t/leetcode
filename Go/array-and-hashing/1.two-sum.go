bd61f91c88570822b6d22db2f37f05d4ea5d228f
Abel Tran <abeltran.develop@gmail.com>
2023-11-05 19:47:39 +0700

    Add two sum - golang

---
+++ Go/array-and-hashing/1.two-sum.go
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1 lang=golang
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+	
+	for i, num := range nums {
+			complement := target - num
+			
+			if j, exists := numMap[complement]; exists {
+					return []int{j, i} 
+			}
+			
+			numMap[num] = i
+	}
+	
+	return nil
+}
+// @lc code=end
+
